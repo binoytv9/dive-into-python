497a2bdc08bd699044e8312e8dba6d58454a7746
binoytv9 <binoytv9@gmail.com>
2015-01-06 13:07:51 +0530

    chapther 4

--- the-power-of-introspection/apihelper.py
+++ the-power-of-introspection/apihelper.py
@@ -1,8 +1,5 @@
 def info(object, spacing=10,collapse=1):
-	"""Print methods and doc strings.
-    
-Takes module, class, list, dictionary, or string."""
-
+	"""Print methods and doc strings.\nTakes module, class, list, dictionary, or string."""
 	methodList = [method for method in dir(object) if callable(getattr(object, method))]
 	processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
 	print "\n".join(["%s %s" %(method.ljust(spacing),processFunc(str(getattr(object, method).__doc__))) 
